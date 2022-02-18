53257002fb59bf8be312d6de81b4e883954f1760
Paripat26 <636051200351@mail.rmutk.ac.th>
2022-02-18 23:48:38 +0700

    Initial commit

---
+++ app/src/main/java/com/example/assignment_quiz/quiz_question_activity.kt
@@ -0,0 +1,11 @@
+package com.example.assignment_quiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class quiz_question_activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz_question)
+    }
+}
